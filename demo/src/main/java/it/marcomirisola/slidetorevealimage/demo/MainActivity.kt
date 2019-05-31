ff78d682aac9294512d7ed5d5759a69582596544
Marco Mirisola <marco.mirisola@ettsolutions.com>
2019-05-31 15:21:47 +0200

    no message

--- demo/src/main/java/it/marcomirisola/slidetorevealimage/demo/MainActivity.kt
+++ demo/src/main/java/it/marcomirisola/slidetorevealimage/demo/MainActivity.kt
@@ -11,12 +11,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        before_after_slider_id.setBaseImage(getDrawable(R.drawable.image))
-//            .addImage(getDrawable(R.drawable.image_bw_1), getDrawable(R.drawable.thumb_image)!!)
-//            .build()
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        before_after_slider_id.setBaseImage(getDrawable(R.drawable.image))
+            .addImage(getDrawable(R.drawable.image_bw_1), getDrawable(R.drawable.thumb_image)!!)
+            .build()
+    }
 }
