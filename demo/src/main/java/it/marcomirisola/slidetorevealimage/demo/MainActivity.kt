a1ce3a2c5424c2287ff69e40a7834ab8ec99739b
Marco Mirisola <marco.mirisola@ettsolutions.com>
2019-05-31 14:27:39 +0200

    merge

--- demo/src/main/java/it/marcomirisola/slidetorevealimage/demo/MainActivity.kt
+++ demo/src/main/java/it/marcomirisola/slidetorevealimage/demo/MainActivity.kt
@@ -6,7 +6,7 @@ import it.marcomirisola.slidetorevealimage.demo.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    
+    //adzfas
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
