e8552f6599ac584f5c94e61d8f3a04c5639b593b
Marco Mirisola <marco.mirisola@ettsolutions.com>
2019-06-28 16:45:14 +0200

    test

--- slidetorevealimage/src/main/java/it/marcomirisola/extensions/Extensions.kt
+++ slidetorevealimage/src/main/java/it/marcomirisola/extensions/Extensions.kt
@@ -4,12 +4,14 @@ import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.request.RequestOptions
 
 /**
  * Created by Jemo on 12/5/16.
  */
 fun ImageView.loadImage(imgUrl: String?){
-    Glide.with(context).load(imgUrl).into(this)
+    Glide.with(context).load(imgUrl).apply(RequestOptions().fitCenter()).into(this)
 }
 
 fun ImageView.loadImage(imgDrawable: Drawable?){
