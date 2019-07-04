8f8829425bb0f4cfe8a60dc13f43e56b522b6648
Marco Mirisola <marco.mirisola@ettsolutions.com>
2019-07-04 10:21:05 +0200

    Squashed commit of the following:
    commit 7c592068ce8f338613626b17b79a9fcf45bb15ee
    Author: Marco Mirisola <marco.mirisola@ettsolutions.com>
    Date:   Fri Jun 28 17:55:57 2019 +0200
    
        WRAP FIXED
    
    commit f171010e1f0094c69e1e1167df6ad88e8bbd9ec3
    Author: Marco Mirisola <marco.mirisola@ettsolutions.com>
    Date:   Fri Jun 28 17:25:40 2019 +0200
    
        no message
    
    commit e8552f6599ac584f5c94e61d8f3a04c5639b593b
    Author: Marco Mirisola <marco.mirisola@ettsolutions.com>
    Date:   Fri Jun 28 16:45:14 2019 +0200
    
        test

--- slidetorevealimage/src/main/java/it/marcomirisola/extensions/Extensions.kt
+++ slidetorevealimage/src/main/java/it/marcomirisola/extensions/Extensions.kt
@@ -1,15 +1,19 @@
 package it.marcomirisola.extensions
 
+import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import android.graphics.drawable.BitmapDrawable
+
+
 
 /**
  * Created by Jemo on 12/5/16.
  */
 fun ImageView.loadImage(imgUrl: String?){
-    Glide.with(context).load(imgUrl).into(this)
+    setImageDrawable(BitmapDrawable(context.resources,  BitmapFactory.decodeFile(imgUrl)))
 }
 
 fun ImageView.loadImage(imgDrawable: Drawable?){
